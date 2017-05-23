499a0169d26e467d4ef64ef9f9e0cb4cba049f00
Michael Hurley <mh@buzzdecafe.com>
2017-05-23 19:08:02 -0400

    break stuff

--- index.js
+++ index.js
@@ -3,45 +3,48 @@ const updating = new Set()
 let isCapturing = false
 const isPrimitive = x => Object(x) !== x
 const toJSON = x => (x.toJSON === undefined ? x : x.toJSON())
-const recompute = node => {
-  node.value = node.compute()
-  node.listeners.forEach(fn => fn(node.value))
-}
 const update = node => {
   if (updating.has(node)) updating.delete(node)
   updating.add(node)
   node.dependents.forEach(update)
 }
-const eachDep = (node, method) =>
-  node.dependencies.forEach(dep => dep.dependents[method](node))
 
-const state = compute => {
-  const node = new_compute => {
+const state = (compute, ds) => {
+  var deps = ds.concat();
+  if (typeof compute !== 'function') {
+    throw new Error('state requires a function argument. Use state.of for other values');
+  }
+
+  const recompute = node => {
+    node.value = node.compute.apply(null, ds);
+    node.listeners.forEach(fn => fn(node.value))
+  }
+  const node = x => {
     if (isCapturing) capturing.add(node)
-    if (new_compute !== undefined) {
-      eachDep(node, 'delete')
-      node.dependencies.clear()
-      node.compute = () => new_compute
-      if (typeof new_compute === 'function') {
-        node.compute = new_compute
-        isCapturing = true
-        capturing.clear()
-        recompute(node)
-        node.dependencies = new Set(capturing)
-        isCapturing = false
-        eachDep(node, 'add')
-      } else {
-        recompute(node)
-      }
-      updating.clear()
-      node.dependents.forEach(update)
-      updating.forEach(recompute)
+    if (x === undefined) {
+      throw new Error('Argument required, none given');
     }
+    node.compute = () => x
+    if (typeof x === 'function') {
+      node.compute = x
+      isCapturing = true
+      capturing.clear()
+      recompute(node)
+      isCapturing = false
+    } else {
+      recompute(node)
+    }
+    updating.clear()
+    node.dependents.forEach(update)
+    updating.forEach(recompute)
+
     return node.value
   }
+
   node.listeners = new Set()
   node.dependents = new Set()
-  node.dependencies = new Set()
+  deps.forEach(dep => dep.dependents.add(node));
+
   node.on = fn => {
     node.listeners.add(fn)
     return () => node.listeners.delete(fn)
@@ -49,7 +52,9 @@ const state = compute => {
   node.toJSON = () =>
     (isPrimitive(node.value) ? node.value : toJSON(node.value))
   node(compute)
+  
   return node
 }
+state.of = x => state(() => x, []);
 
 module.exports = state
